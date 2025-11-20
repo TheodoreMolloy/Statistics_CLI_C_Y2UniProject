f687544ffe5e7fced5438f2fa42bcf4a36e509fe
Theo Molloy <theodorexmolloy@gmail.com>
2025-11-20 11:50:13 +0000

    Added main.c template

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include "funcs.h"
 
+int main(void) {
+/* this will run forever until we call exit(0) in select_menu_item() */
+  for(;;) {
+    main_menu();
+  }
+  /* not reached */
+  return 0;
+}
